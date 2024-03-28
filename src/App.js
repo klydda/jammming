977ff2e0ea79679041b0d8dfe519e8b953824e8b
klydda <klydda@proton.me>
2024-03-28 14:29:55 +0100

    Refactor to use App instead of SearchContainer

--- src/App.js
+++ src/App.js
@@ -1,13 +1,40 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 import SearchBar from './Components/SearchBar';
-import SearchContainer from './Containers/SearchContainer';
+import SearchResults from './Components/SearchResults';
 
 function App() {
+  const [search, setSearch] = useState('');
+  const [url, setUrl] = useState('');
+
+  function handleSearchInput(e) {
+      setSearch(e.target.value);
+  }
+
+  function handleSubmit(e) {
+      e.preventDefault();
+      const url = constructUrl();
+      setUrl(url);
+  }
+
+  function constructUrl() {
+      const baseUrl = 'part1'
+      const searchQuery = search;
+      return `${baseUrl}${searchQuery}`;
+  }
+
   return (
-    <div className="App">
-      <SearchContainer />
-    </div>
+      <div className="App">
+          <SearchBar
+              search={search}
+              onSearch={handleSearchInput}
+              onSubmit={handleSubmit}
+          />
+          <SearchResults 
+              url={url}
+          />
+      </div>
+      
   );
 }
 
