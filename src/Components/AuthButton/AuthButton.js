b1d8ce7d66b2894a2e7f42b86c978e2ef5dd645c
klydda <klydda@proton.me>
2024-03-31 17:51:35 +0200

    Add authbutton and auth state

---
+++ src/Components/AuthButton/AuthButton.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './AuthButton.module.css';
+
+function AuthButton({onClick}){
+    return (
+        <div className={styles.authWrapper}>
+            <button onClick={onClick}>Connect your Spotify Account</button>
+        </div>
+    );
+}
+
+export default AuthButton;
