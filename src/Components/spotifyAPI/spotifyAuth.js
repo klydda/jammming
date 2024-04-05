8b415a32a6402ea572c746831f9642104efd2d5f
klydda <klydda@proton.me>
2024-04-05 10:24:03 +0200

    Add conditional redirect for localhost vs public deploy

--- src/Components/spotifyAPI/spotifyAuth.js
+++ src/Components/spotifyAPI/spotifyAuth.js
@@ -1,5 +1,6 @@
 const client_id = '88ae3e12bdf1469d8cd50cec3ecb2172';
-const redirect_uri = 'http://localhost:3000/';
+const redirect_uri_local = 'http://localhost:3000/';
+const redirect_uri_public = 'https://klyddajammm.netlify.app/';
 
 const scope = 'user-read-private user-read-email playlist-modify-public playlist-modify-private';
 
@@ -7,7 +8,13 @@ let url = 'https://accounts.spotify.com/authorize';
 url += '?response_type=token';
 url += '&client_id=' + encodeURIComponent(client_id);
 url += '&scope=' + encodeURIComponent(scope);
-url += '&redirect_uri=' + encodeURIComponent(redirect_uri);
+
+if(window.location === redirect_uri_local){
+    url += '&redirect_uri=' + encodeURIComponent(redirect_uri_local);
+} else {
+    url += '&redirect_uri=' + encodeURIComponent(redirect_uri_public);
+}
+
 
 console.log(url);
 
